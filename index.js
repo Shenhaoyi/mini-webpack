e3d7de7278e9228217ff9d0c8fb9e6bb9d8fa442
Shenhaoyi <shenhaoyi04@163.com>
2023-09-13 19:37:12 +0800

    生成ast

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import fs from 'fs';
+import parser from '@babel/parser';
+
 
 function createAsset() {
   // 1.获取文件内容
   const source = fs.readFileSync('./example/main.js', {
     encoding: 'utf-8',
   });
+
   // 2.获取依赖关系
 
+  // 生成ast树
+  const ast = parser.parse(source, {
+    sourceType: 'module' // 需要指定代码的模块规范
+  });
 
   return {};
 }
